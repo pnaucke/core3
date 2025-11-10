f2d8f3c2ccf673f2554a1fcb75a5ed0a66bb0ba3
Pieter Naucke <554603@student.fontys.nl>
2025-11-10 16:27:00 +0100

    v1.1

---
+++ main.tf
@@ -0,0 +1,19 @@
+terraform {
+  backend "s3" {
+    bucket  = "innovatech-terraform-state"
+    key     = "terraform.tfstate"
+    region  = "eu-central-1"
+    encrypt = true
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-central-1"
+}
