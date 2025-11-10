4a97d20b292d577e01c95c085ba47a2d48f2ac2d
Pieter Naucke <554603@student.fontys.nl>
2025-11-10 17:50:56 +0100

    v1.14

--- network.tf
+++ network.tf
@@ -45,3 +45,34 @@ resource "aws_subnet" "db_subnet2" {
   map_public_ip_on_launch = false
   tags = { Name = "db-subnet-2" }
 }
+
+# ----------------------
+# Internet Gateway
+# ----------------------
+resource "aws_internet_gateway" "main_igw" {
+  vpc_id = aws_vpc.main_vpc.id
+  tags = { Name = "main-igw" }
+}
+
+# ----------------------
+# Route Table voor public subnets
+# ----------------------
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main_igw.id
+  }
+  tags = { Name = "public-rt" }
+}
+
+# Associate route table met public subnets
+resource "aws_route_table_association" "web1_assoc" {
+  subnet_id      = aws_subnet.web1_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "web2_assoc" {
+  subnet_id      = aws_subnet.web2_subnet.id
+  route_table_id = aws_route_table.public_rt.id
+}
