12463f5217ed4d268ec2f1aafb7f3d36df27e09b
Pieter Naucke <554603@student.fontys.nl>
2025-11-11 13:51:09 +0100

    v1.23

--- outputs.tf
+++ outputs.tf
@@ -1,12 +1,11 @@
 output "web1_public_ip" {
-  value = aws_instance.webserver1.public_ip
+  value = aws_instance.web1.public_ip
 }
 
 output "web2_public_ip" {
-  value = aws_instance.webserver2.public_ip
+  value = aws_instance.web2.public_ip
 }
 
 output "db_endpoint" {
   value = aws_db_instance.db.address
 }
-
