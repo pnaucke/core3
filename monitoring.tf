9cc583fdf973d48977e3c091aeeccab467405d1c
Pieter Naucke <554603@student.fontys.nl>
2025-11-27 09:56:10 +0100

    v5.23

--- monitoring.tf
+++ monitoring.tf
@@ -1,72 +1,129 @@
+# Log group voor VPC Flow Logs
+resource "aws_cloudwatch_log_group" "vpc_flowlogs" {
+  name              = "/aws/vpc/flowlogs"
+  retention_in_days = 7
+}
+
+# IAM role voor flow logs
+resource "aws_iam_role" "flowlog_role" {
+  name = "flowlog_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "vpc-flow-logs.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+# IAM policy
+resource "aws_iam_role_policy" "flowlog_policy" {
+  name = "flowlog_policy"
+  role = aws_iam_role.flowlog_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ],
+        Resource = "${aws_cloudwatch_log_group.vpc_flowlogs.arn}:*"
+      }
+    ]
+  })
+}
+
+# Flow logs voor VPC
+resource "aws_flow_log" "vpc_flow" {
+  log_destination      = aws_cloudwatch_log_group.vpc_flowlogs.arn
+  log_destination_type = "cloud-watch-logs"
+  traffic_type         = "ALL"
+  vpc_id               = aws_vpc.main.id
+  iam_role_arn         = aws_iam_role.flowlog_role.arn
+}
+
+# Metric filter voor SSH pogingen
+resource "aws_cloudwatch_log_metric_filter" "ssh_unauthorized_filter" {
+  name           = "ssh_unauthorized"
+  log_group_name = aws_cloudwatch_log_group.vpc_flowlogs.name
+
+  pattern = "{ $.dstPort = 22 }"
+
+  metric_transformation {
+    name      = "ssh_unauthorized_count"
+    namespace = "custom"
+    value     = "1"
+  }
+}
+
+# Alarm CPU hoog
 resource "aws_cloudwatch_metric_alarm" "cpu_high_web" {
   alarm_name          = "cpu_high_web"
   comparison_operator = "GreaterThanThreshold"
   evaluation_periods  = 1
-  metric_name         = "CPUUtilization"
-  namespace           = "AWS/ECS"
+  threshold           = 80
   period              = 60
   statistic           = "Average"
-  threshold           = 80
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/ECS"
 
   dimensions = {
     ClusterName = aws_ecs_cluster.webcluster.name
     ServiceName = aws_ecs_service.webservice.name
   }
 
-  alarm_actions = []
+  alarm_description = "CPU boven tachtig procent"
 }
 
-resource "aws_cloudwatch_log_metric_filter" "ssh_unauthorized_filter" {
-  name           = "ssh_unauthorized_filter"
-  pattern        = "{ $.eventName = \"AuthorizeSecurityGroupIngress\" }"
-  log_group_name = "/aws/vpc/flowlogs"
-
-  metric_transformation {
-    name      = "ssh_unauthorized_count"
-    namespace = "CustomSecurity"
-    value     = "1"
-  }
-}
-
+# Alarm SSH unauthorized
 resource "aws_cloudwatch_metric_alarm" "ssh_unauthorized_alarm" {
   alarm_name          = "ssh_unauthorized_alarm"
   comparison_operator = "GreaterThanThreshold"
   evaluation_periods  = 1
-  metric_name         = "ssh_unauthorized_count"
-  namespace           = "CustomSecurity"
+  threshold           = 1
   period              = 60
   statistic           = "Sum"
-  threshold           = 0
-  alarm_actions       = []
+  metric_name         = "ssh_unauthorized_count"
+  namespace           = "custom"
+
+  alarm_description = "SSH poging"
 }
 
+# Dashboard
 resource "aws_cloudwatch_dashboard" "main_dashboard" {
-  dashboard_name = "main_dashboard"
+  dashboard_name = "web_monitoring"
 
   dashboard_body = jsonencode({
     widgets = [
       {
         type = "metric"
+        width = 12
+        height = 6
         properties = {
           metrics = [
-            [ "AWS/ECS", "CPUUtilization", "ClusterName", aws_ecs_cluster.webcluster.name, "ServiceName", aws_ecs_service.webservice.name ]
+            [ "AWS", "ECS", "CPUUtilization", "ClusterName", aws_ecs_cluster.webcluster.name, "ServiceName", aws_ecs_service.webservice.name ]
           ]
-          period = 60
-          stat = "Average"
-          region = "eu-central-1"
-          title = "CPU Webserver"
+          title = "CPU gebruik"
         }
       },
       {
         type = "metric"
+        width = 12
+        height = 6
         properties = {
           metrics = [
-            [ "CustomSecurity", "ssh_unauthorized_count" ]
+            [ "custom", "ssh_unauthorized_count" ]
           ]
-          period = 60
-          stat = "Sum"
-          region = "eu-central-1"
-          title = "SSH Unauthorized Attempts"
+          title = "SSH pogingen"
         }
       }
     ]
