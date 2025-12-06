d1b568004a4ed7d1c56bf624c32004c4d13c354b
pnaucke <nauckepieter@gmail.com>
2025-12-06 15:52:58 +0100

    v6.27

--- network.tf
+++ network.tf
@@ -3,6 +3,9 @@ resource "aws_vpc" "main" {
   cidr_block       = "172.31.0.0/16"
   instance_tenancy = "default"
 
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
   tags = {
     Name = "VPC-HR"
   }
