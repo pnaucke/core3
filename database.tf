c0e1649a107ad95aad6fd19356f17f56bb5a0412
pnaucke <nauckepieter@gmail.com>
2025-12-07 16:26:16 +0100

    v7.5

--- database.tf
+++ database.tf
@@ -17,15 +17,18 @@ resource "aws_db_instance" "hr_database" {
   allocated_storage    = 20
   storage_type         = "gp2"
   
-  # Credentials - rechtstreeks ingevuld voor test
+  # Database naam
+  db_name = "innovatech"
+  
+  # Credentials
   username = "admin"
-  password = "admin123!"  # Hardcoded voor test
+  password = "admin123!"
   
   # Network configuratie
   db_subnet_group_name   = aws_db_subnet_group.hr_db_subnet_group.name
   vpc_security_group_ids = [aws_security_group.sg_database.id]
   
-  # Externe toegang (dit geeft de database een public IP)
+  # Externe toegang
   publicly_accessible    = true
   
   # General
@@ -33,5 +36,35 @@ resource "aws_db_instance" "hr_database" {
   
   tags = {
     Name = "hr-database"
+  }
+}
+
+# Tabbles
+resource "null_resource" "create_tables" {
+  depends_on = [aws_db_instance.hr_database]
+
+  provisioner "local-exec" {
+    command = <<EOT
+      mysql -h ${aws_db_instance.hr_database.endpoint} -u admin -p'admin123!' -e "
+        CREATE DATABASE IF NOT EXISTS innovatech;
+        USE innovatech;
+        
+        CREATE TABLE IF NOT EXISTS users (
+          id INT(5) AUTO_INCREMENT PRIMARY KEY,
+          name VARCHAR(50),
+          email VARCHAR(50),
+          department VARCHAR(50),
+          status VARCHAR(50),
+          role VARCHAR(50)
+        );
+        
+        CREATE TABLE IF NOT EXISTS hr (
+          name VARCHAR(50),
+          password VARCHAR(50)
+        );
+        
+        INSERT INTO hr (name, password) VALUES ('admin', 'admin123');
+      "
+    EOT
   }
 }
